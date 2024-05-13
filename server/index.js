5f60546b450ea57175dc43900fb2d7fe7efa56eb
varishaa-scaler <varisha.rashid@scaler.com>
2024-05-13 21:52:17 +0530

    deployment changes

--- server/index.js
+++ server/index.js
@@ -23,6 +23,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+}
+
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
